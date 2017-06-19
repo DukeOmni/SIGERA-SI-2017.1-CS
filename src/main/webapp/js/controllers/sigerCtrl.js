ed307fd7831f7d4b92cb25ae7c808c8f46efee1c
Alex Cruzeiro <alexcruzeiro@rocketmail.com>
2017-06-19 15:03:01 -0300

    Ajuste da tela de ver alunos

--- src/main/webapp/js/controllers/sigerCtrl.js
+++ src/main/webapp/js/controllers/sigerCtrl.js
@@ -1,11 +1,16 @@
 angular.module("siger").controller("sigerCtrl", function($scope, $location, $rootScope){
     $scope.alunos = $rootScope.alunosCadastrados;
     $scope.alunoSelecionado;
-    $scope.mostrarAluno = function(aluno){
-        $scope.alunoSelecionado = aluno;
-    };
-    $scope.fecharAluno = function(){
+    var fecharAluno = function(){
+        console.log("Entrou aqui");
         $scope.alunoSelecionado = false;
+    };
+    $scope.mostrarAluno = function(aluno){
+       if($scope.alunoSelecionado == aluno){
+            fecharAluno();
+        }else{
+        $scope.alunoSelecionado = aluno;
+        }
     };
 });
 

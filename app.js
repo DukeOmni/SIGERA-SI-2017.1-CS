83e42896e3cd0de5859b26e017d4102f27559a3c
DukeOmni <caiordsilva@gmail.com>
2017-06-25 00:38:23 -0300

    Arrumando bug no banco

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var configDb = require('./config');
 var setupAlunos = require('./controllers/setupControllerAlunos');
 var setupRotas = require('./controllers/setupControllerRotas');
-var apiController = require('./controllers/apiController');
+var apiController = require('./controllers/apiControllerAlunos');
 var app = express();
 var port = process.env.port || 3000;
 
